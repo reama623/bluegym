b8a8d5d631586487a34eccd3dbe80446ec270dd5
jayden <reama623@gmail.com>
2022-03-15 01:16:16 +0900

    add select today exercise api

--- pages/api/todayexercise/index.js
+++ pages/api/todayexercise/index.js
@@ -5,6 +5,8 @@ export default async function handler(req, res) {
   const { query, method, body } = req;
 
   if (method === "GET") {
+    const d = await selectTodayExeriseOfUser(query)
+    return res.status(200).json(d);
   }
 
   if (method === "POST") {
@@ -12,7 +14,26 @@ export default async function handler(req, res) {
 
     return res.status(201).json(d);
   }
+  return res.status(200).json([]);
 }
+
+const selectTodayExeriseOfUser = async ({
+  member_id,
+  trainer_id,
+  group_name,
+  start_date,
+  end_date,
+}) => {
+  const q = `select te.seq, te.date,e.name, te.exercise_desc from today_exercise te join exercise e on e.seq = te.exercise_seq where member_id=? and te.trainer_id=? and group_name=? and date between ? and ?`;
+  const d = await request(q, [
+    member_id,
+    trainer_id,
+    group_name,
+    start_date,
+    end_date,
+  ]);
+  return d;
+};
 
 const insertTodayExercise = async (exercises) => {
   const values = exercises
@@ -33,7 +54,7 @@ const insertTodayExercise = async (exercises) => {
         )}, ${exercise_type})`
     )
     .join(",");
-  const query = `insert into today_exercise (member_id, trainer_id, group_name, exercise_seq, date, exercise_desc, exercise_type) values ${values}`;
-  const d = await request(query);
+  const q = `insert into today_exercise (member_id, trainer_id, group_name, exercise_seq, date, exercise_desc, exercise_type) values ${values}`;
+  const d = await request(q);
   return d;
 };
